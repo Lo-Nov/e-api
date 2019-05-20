e7eb471c5781f99ed99869db1f8cf8e8fbf2e2f1
Revparking <ozzaynetwork@gmail.com>
2019-05-20 15:38:51 +0300

    Review created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -113,7 +113,7 @@ class ProductController extends Controller
     public function destroy(Product $product)
     {
         $this->ProductUserCheck($product);
-        
+
         $product->delete();
 
         return response(null, Response::HTTP_NO_CONTENT);
